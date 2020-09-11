c20e7e1e287abc0f8ea658e815baf755c0d25268
Roman-Freelancer <70280915+Roman-Freelancer@users.noreply.github.com>
2020-09-11 06:37:14 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//Проверяем на наличие формата WEBP==================================================================
+function testWebP(callback) {
+  var webP = new Image();
+
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
+}); //JS-==================================================
+
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.header__list, .header__bottom").toggleClass("active");
+    $("html").toggleClass("lock");
+  });
+  $(".sport__slider").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  });
+});
